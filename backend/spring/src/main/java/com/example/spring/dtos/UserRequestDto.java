dcbd80cc8ed6df567c8665c5092c9b8195673092
Ezequiel-Buzzella <ezequiel.buzzella@gmail.com>
2025-11-05 16:49:05 -0300

    starting with userDTO

--- backend/spring/src/main/java/com/example/spring/dtos/UserRequestDto.java
+++ backend/spring/src/main/java/com/example/spring/dtos/UserRequestDto.java
@@ -1,8 +1,20 @@
 package com.example.spring.dtos;
 
+import org.springframework.stereotype.Indexed;
+
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Data
-public class UserRequestDto {
+public class UserRequestDto {   
+
+    
+    private String name;
+    private String surname;
+
+
+
 }
 // This class is currently empty, but it can be used to define user request data transfer objects in the future.
