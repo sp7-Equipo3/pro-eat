63c859e79035febd97e78775f151a4c01ed41191
Ricardo Quejuan <quejuan52@gmail.com>
2025-11-06 18:20:04 -0500

    feat(productos): agregar consumo de API y manejo de estados

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
+import { Products } from "./assets/pages/products"
 
 function App() {
 
   return (
     <>
-      <div className="w-lvw h-lvh flex justify-center items-center flex-col bg-black">
-        <h1 className="text-white text-5xl">Hello World</h1>
+      <div className="min-h-screen bg-gray-100">
+        <Products/>
       </div>
     </>
   )
