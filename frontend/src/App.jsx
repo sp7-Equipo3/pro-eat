a9c09ef83ea08f9467df9d04e33debaf07d44c28
Inés Coromoto Sánchez Freites <125275762+Inesc28@users.noreply.github.com>
2025-11-06 19:31:58 -0400

    Revert "4 estructurar rutas y formularios iniciales"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,13 @@
-import { RouterProvider } from "react-router-dom";
-import  router  from "@/infrastructure/router/routes.config";
 
 function App() {
-  return <RouterProvider router={router} />;
+
+  return (
+    <>
+      <div className="w-lvw h-lvh flex justify-center items-center flex-col bg-black">
+        <h1 className="text-white text-5xl">Hello World</h1>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 import App from './App.jsx'
-import { queryClient } from './lib/query-client.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-        <App />
-    </QueryClientProvider>
+    <App />
   </StrictMode>,
 )
