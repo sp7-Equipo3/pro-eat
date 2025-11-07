b19df12f9a2b1d18495ee9e7298fec89b97e7bdc
Sonny <spimentelm1201@gmail.com>
2025-11-07 11:47:16 -0500

    Corregir mapeo de UserController con @RequestMapping

--- backend/spring/src/main/java/com/example/spring/controllers/UserController.java
+++ backend/spring/src/main/java/com/example/spring/controllers/UserController.java
@@ -1,9 +1,11 @@
 package com.example.spring.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/")
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
 
     @GetMapping("/hola")
