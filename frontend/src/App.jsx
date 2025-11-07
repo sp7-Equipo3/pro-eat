d997a467683bdbc349a6a226ecdc573f41124434
Jorge Trujillo <39315478+xiriuxb@users.noreply.github.com>
2025-11-06 19:34:28 -0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
-import { Products } from "./assets/pages/products"
+
 import { RouterProvider } from "react-router-dom";
 import  router  from "@/infrastructure/router/routes.config";
+import { Products } from "./assets/pages/Products";
 
 function App() {
 
   return (
     <>
       <div className="min-h-screen bg-gray-100">
-        <Products/>
+        <Products />
       </div>
     <RouterProvider router={router} />;
     </>
